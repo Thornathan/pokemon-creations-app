7240efd4bc7d318e10e5a0455a0b743f43ecf0cd
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-07-14 13:22:17 -0700

    Added Back-end folders and setup mongoose database.js

---
+++ config/database.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+mongoose.connect(
+  'mongodb://localhost:27017/records',
+  { useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+);
+var db = mongoose.connection;
+db.on('connected', function() {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
