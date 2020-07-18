9fe5ffeb6b0cd11596f8a1066bb1cc45f978ba2d
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-07-17 18:32:40 -0700

    Setup Pokemon List Page for API

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PokemonList from '../pokeApi/PokemonList'
+import SearchBar from '../search/SearchBar';
 
 export default class Dashboard extends Component {
   render() {
--- src/components/pokeApi/PokemonList.js
+++ src/components/pokeApi/PokemonList.js
@@ -1,31 +1,37 @@
-import React, { Component } from "react";
-import axios from "axios";
-import PokemonCard from "./PokemonCard";
+import React, { Component } from 'react';
+
+import PokemonCard from './PokemonCard';
+import Loading from '../layout/Loading';
+import axios from 'axios';
 
 export default class PokemonList extends Component {
   state = {
-    url: "https://pokeapi.co/api/v2/pokemon/",
-    pokemon: null,
+    url: 'https://pokeapi.co/api/v2/pokemon/',
+    pokemon: null
   };
 
   async componentDidMount() {
     const res = await axios.get(this.state.url);
-    this.setState({ pokemon: res.data["results"] });
+    this.setState({ pokemon: res.data['results'] });
   }
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         {this.state.pokemon ? (
           <div className="row">
             {this.state.pokemon.map(pokemon => (
-              <PokemonCard />
+              <PokemonCard
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+              />
             ))}
           </div>
-          ) : (
-          <h1>Loading Pokemon</h1>
-          )}
-      </React.Fragment>
-      );
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
   }
 }
