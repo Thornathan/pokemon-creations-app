214ead8e1ac971729091591e16d49b3f4403184b
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-07-19 10:54:05 -0700

    Finished Pokedex

--- src/components/pokeApi/PokemonList.js
+++ src/components/pokeApi/PokemonList.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 export default class PokemonList extends Component {
   state = {
-    url: 'https://pokeapi.co/api/v2/pokemon/',
+    url: 'https://pokeapi.co/api/v2/pokemon/?limit=964',
     pokemon: null
   };
 
